2d566fe1378397af4fc162597760f3cc2eb43ea3
ashishpatel3012 <129655846+ashishpatel3012@users.noreply.github.com>
2024-09-08 03:53:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { Page2 } from './components/page2'
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <section className='homepage'>
+    <div ><img src="./images/dices.png" alt="" /></div>
+    <div>
+      <h1>DICE GAME</h1>
+      <button>Play now</button>
+    </div>
+    </section>
+    <Page2/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/page2.jsx
@@ -0,0 +1,28 @@
+
+import "./page2.css"
+
+
+export const Page2 = () => {
+  return (
+    <>
+    <section className="page2nav">
+    <div>
+        <h1>0</h1>
+        <p>Total Score</p>
+    </div>
+
+    <div className="btnsnav"> 
+       <div className="btns">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        </div>
+        <p>Select Number</p>
+    </div>
+    </section>
+    </>
+  )
+}
